e0a4e3f3fa5cce01b6ff77f590d63940609c49e5
Calvinbullock <calvinnbullock@gamil.com>
2023-05-11 14:42:08 -0600

    implemented the hamburger menu java script

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -1,1 +1,14 @@
-const fulldate = new Intl.DateTimeFormat("en-us", {}); // unfinished
+// Curent date and time
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format( new Date() );
+
+// Hamburger meny
+toggleButton = document.querySelector("#menu-toggle");
+
+toggleButton.addEventListener('click', (event) => {
+    navbar = document.querySelector("#navbar");
+    openSpan = document.querySelector("#menu-open");
+    closeSpan = document.querySelector("#menu-close")
+    navbar.classList.toggle('open');
+    openSpan.classList.toggle('open');
+    closeSpan.classList.toggle('open');
+});
