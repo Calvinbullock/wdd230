ddbb0bc8e6f93d322ef278b11780b107b2ed9174
Calvinbullock <calvinnbullock@gamil.com>
2023-05-13 10:10:56 -0600

    finished up some formating and styling

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -2,7 +2,10 @@
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format( new Date() );
 document.querySelector(".current-time").textContent = fulldate;
 
-// Hamburger meny
+// This sets the last modified date on the home page
+document.querySelector(".lastupdated").textContent = document.lastModified;
+
+// Hamburger menu
 toggleButton = document.querySelector("#menu-toggle");
 
 toggleButton.addEventListener('click', (event) => {
