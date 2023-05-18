ef9ff31d72d34a2984692a3428396ad53f298a72
Calvinbullock <calvinnbullock@gamil.com>
2023-05-18 10:54:16 -0600

    fixed copy right year in header-footer.js

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -5,6 +5,10 @@ document.querySelector(".current-time").textContent = fulldate;
 // This sets the last modified date on the home page
 document.querySelector(".lastupdated").textContent = document.lastModified;
 
+// This sets the year for the footer
+const today = new Date();
+document.querySelector(".currentyear").textContent = today.getFullYear();
+
 // Hamburger menu
 toggleButton = document.querySelector("#menu-toggle");
 
