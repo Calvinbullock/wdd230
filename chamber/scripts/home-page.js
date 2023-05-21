4e359ff6aaf4aaee1801ab467a017da47dc6c652
Calvinbullock <calvinnbullock@gamil.com>
2023-05-20 18:55:56 -0600

    js update

--- chamber/scripts/home-page.js
+++ chamber/scripts/home-page.js
@@ -1,7 +1,7 @@
 // Show the meet-greet message on Mon/Tue
 // Date.getDay() uses a 0 based index to return the day of the week
 var messagedate = new Date();
-// if (messagedate.getDay()==1 || messagedate.getDay()==2 ){
-if (messagedate.getDay()!=1 || messagedate.getDay()!=2 ){ // Testing if REMOVE
+if (messagedate.getDay()==1 || messagedate.getDay()==2 ){
+// if (messagedate.getDay()!=1 || messagedate.getDay()!=2 ){ // Testing if REMOVE
 document.querySelector("#meet-greet").classList.add('active');
 }
