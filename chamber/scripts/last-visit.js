2989da576b4320cf9411c9f5ef09af55db080f51
Calvinbullock <calvinnbullock@gamil.com>
2023-06-01 13:56:35 -0600

    broken js for last visit

--- chamber/scripts/last-visit.js
+++ chamber/scripts/last-visit.js
@@ -1,14 +1,16 @@
-// const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
-let today = new Date();
+const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
+let todaysVisit = new Date();
 let lastVisitStr = localStorage.getItem("lastVisit");
 let lastVisitSpan = document.querySelector("last-vist");
+console.log("ohi")
 
-if (lastVisitStr==null){        
+if (lastVisitStr == null) {
     lastVisitSpan.textContent = '0'
-}
-else{
-    lastVisitDate=new Date(lastVisitStr);
-    daysSinceLastVisit = Math.floor((today.getTime() - lastVisitDate.getTime()) / MILLIS_PER_DAY);
+    
+} else {
+    lastVisitDate = new Date(lastVisitStr);
+    daysSinceLastVisit = Math.floor((todaysVisit.getTime() - lastVisitDate.getTime()) / MILLISECONDS_PER_DAY);
     lastVisitSpan.textContent = daysSinceLastVisit;
 }
-localStorage.setItem("lastVisit", today.toLocaleDateString());
+
+localStorage.setItem("lastVisit", todaysVisit.toLocaleDateString());
