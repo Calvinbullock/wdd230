9de2b2df3de80dbb9e4864d5902f5a7378179098
Calvinbullock <calvinnbullock@gamil.com>
2023-06-01 14:01:32 -0600

    added a comment to lazy loader js

--- chamber/scripts/last-visit.js
+++ chamber/scripts/last-visit.js
@@ -2,9 +2,11 @@ const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000;
 let todaysVisit = new Date();
 let lastVisitStr = localStorage.getItem("lastVisit");
 let lastVisitSpan = document.querySelector("last-vist");
-console.log("ohi")
+console.log("1")
 
 if (lastVisitStr == null) {
+    // breaks here
+    console.log("2")
     lastVisitSpan.textContent = '0'
     
 } else {
