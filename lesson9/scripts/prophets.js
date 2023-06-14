aaa7446f9718cf2d90257b5b82515980a7e740f9
Calvinbullock <calvinnbullock@gamil.com>
2023-06-14 09:42:36 -0600

    prophets save point all data is displayed

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -2,10 +2,14 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophet
 
 async function getProphetData() {
     const response = await fetch(url);
-    const data = await response.json();
-    console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
-    displayProphets(data.prophets);
-
+    if (response.ok) {
+        const data = await response.json();
+        displayProphets(data.prophets);
+    } else {
+        console.error("There was an error loading the data.");
+        const cards = document.querySelector("div.cards");
+        cards.innerHTML = "<section><h1>There was an error loading the data</h1></section>";
+    }
 }
 
 const displayProphets = (prophets) => {
@@ -14,24 +18,15 @@ const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
         // Create elements to add to the div.cards element
         let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let portrait = document.createElement('img');
 
-        // Build the h2 content out to show the prophet's full name - finish the template string
-        h2.textContent = `${prophet.name} ${prophet.lastname}`;
-
-        // Build the image portrait by setting all the relevant attribute
-        portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
-
-        // Append the section(card) with the created elements
-        card.appendChild(h2);
-        card.appendChild(portrait);
-
+        // Parse all the perstions info to html 
+        card.innerHTML = `<h2>${prophet.name} ${prophet.lastname}</h2>
+      <h3>Date of Birth: ${prophet.birthdate}</h3>
+      <h3>Place of Birth: ${prophet.birthplace}</h3>
+      <img src="${prophet.imageurl}" height="440" width="340" loading="lazy" alt="Portrait of ${prophet.name} ${prophet.lastname}">`;
         cards.appendChild(card);
+
     }) // end of forEach loop
-  } // end of function expression
+} // end of function expression
+
 getProphetData();
